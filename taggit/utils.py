2a2538bd01d651706916f4c4fa4c244a72e67329
Alfredo Aguirre <alfredo@madewithbyt.es>
2014-05-29 21:16:03 +0200

    Use only commas to generate tags.

--- taggit/utils.py
+++ taggit/utils.py
@@ -6,74 +6,7 @@ from django.utils import six
 
 
 def parse_tags(tagstring):
-    """
-    Parses tag input, with multiple word input being activated and
-    delineated by commas and double quotes. Quotes take precedence, so
-    they may contain commas.
-
-    Returns a sorted list of unique tag names.
-
-    Ported from Jonathan Buchanan's `django-tagging
-    <http://django-tagging.googlecode.com/>`_
-    """
-    if not tagstring:
-        return []
-
-    tagstring = force_text(tagstring)
-
-    # Special case - if there are no commas or double quotes in the
-    # input, we don't *do* a recall... I mean, we know we only need to
-    # split on spaces.
-    if ',' not in tagstring and '"' not in tagstring:
-        words = list(set(split_strip(tagstring, ' ')))
-        words.sort()
-        return words
-
-    words = []
-    buffer = []
-    # Defer splitting of non-quoted sections until we know if there are
-    # any unquoted commas.
-    to_be_split = []
-    saw_loose_comma = False
-    open_quote = False
-    i = iter(tagstring)
-    try:
-        while True:
-            c = six.next(i)
-            if c == '"':
-                if buffer:
-                    to_be_split.append(''.join(buffer))
-                    buffer = []
-                # Find the matching quote
-                open_quote = True
-                c = six.next(i)
-                while c != '"':
-                    buffer.append(c)
-                    c = six.next(i)
-                if buffer:
-                    word = ''.join(buffer).strip()
-                    if word:
-                        words.append(word)
-                    buffer = []
-                open_quote = False
-            else:
-                if not saw_loose_comma and c == ',':
-                    saw_loose_comma = True
-                buffer.append(c)
-    except StopIteration:
-        # If we were parsing an open quote which was never closed treat
-        # the buffer as unquoted.
-        if buffer:
-            if open_quote and ',' in buffer:
-                saw_loose_comma = True
-            to_be_split.append(''.join(buffer))
-    if to_be_split:
-        if saw_loose_comma:
-            delimiter = ','
-        else:
-            delimiter = ' '
-        for chunk in to_be_split:
-            words.extend(split_strip(chunk, delimiter))
+    words = [t for t in tagstring.split(',') if t]
     words = list(set(words))
     words.sort()
     return words
